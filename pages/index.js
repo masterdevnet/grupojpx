692c51cf87c1e953d049221e6c45aebcdcfb5010
EstevamNet <masterdev.net@gmail.com>
2021-11-10 16:54:02 -0300

    Adicionado componente Head

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
+import Head from 'next/head'
+
 function Home() {
     return (
         <div>
-            <h1>Bem vindo ao NextJS</h1>
+            <Head>
+                <meta charset="utf-8" />
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                <meta name="robots" content="index, follow" />
+                <meta name="description" content="Site do cgrupo JPX" />
+                <meta name="author" content="Manoel Estevam MasterDev" />
+                <title>GRUPO JPX</title>
+            </Head>
+            <h1>GRUPO JPX</h1>
+            <h3>Bem vindo ao novo site do grupo JPX</h3>
         </div>
     )
 }
