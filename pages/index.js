5f21aa6f244561bf9a7289f35e7045f5fca0edce
EstevamNet <masterdev.net@gmail.com>
2021-11-11 09:48:37 -0300

    Corrigido arquivo index.js

--- pages/index.js
+++ pages/index.js
@@ -13,9 +13,6 @@ function Home() {
                 <meta name="author" content="Manoel Estevam MasterDev" />
                 <title>GRUPO JPX - BEM VINDO</title>
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-                <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;1,700&display=swap" rel="stylesheet" /> 
             </Head>
             <Menu />
             {/* <Navbar /> */}
